c89b8b5b22fe1005b9b3aeab34f2c0a7b35d74e0
정동구/연구원/MC Future Product Lab 2팀 1파트(dg.jung@lge.com) <dg.jung@lge.com>
2017-07-19 09:45:05 +0900

    set PORT

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-
-// http server를 socket.io server로 upgrade.
 var io = require('socket.io')(server);
+
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -33,6 +33,6 @@ io.on('connection', function(socket){
 
 });
 
-server.listen(3000, function() {
+server.listen(port, function() {
     console.log('Socket IO server listening on port 3000');
 });
