861e0155156a44df0038f95b7edbf7b6a5245b9d
정동구/연구원/MC Future Product Lab 2팀 1파트(dg.jung@lge.com) <dg.jung@lge.com>
2017-07-21 11:40:29 +0900

    change

--- route.js
+++ route.js
@@ -15,11 +15,7 @@ router.get('/', function(req, res) {
     api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
     
     res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-    res.end(
-        "<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/big_g.PNG'/></a>",
-    );
-
-
+    res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/big_g.PNG'/></a>");
 });
 
 router.get('/callback', function (req, res) {
