f07d6ce04e73ca030ebc4d61a474a17443969a05
정동구/연구원/MC Future Product Lab 2팀 1파트(dg.jung@lge.com) <dg.jung@lge.com>
2017-07-21 11:29:41 +0900

    anonymous login

--- static/js/naverLogin.js
+++
@@ -1,7 +0,0 @@
-var naver_id_login = new naver_id_login("gFcZMD8oSVXpwjzDiOuF", "5AnI3RgpSM");
-var state = naver_id_login.getUniqState();
-naver_id_login.setButton("white", 2,40);
-naver_id_login.setDomain("http://localhost:3000");
-naver_id_login.setState(state);
-naver_id_login.setPopup();
-naver_id_login.init_naver_id_login();
